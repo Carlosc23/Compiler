f20f1a857c25e7512dff882ac852510030a07e17
Carlos <carlos.calderon097@gmail.com>
2018-01-17 22:51:45 -0600

    Preliminar GUI with 2 tabs.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Compiler");
+        primaryStage.setScene(new Scene(root, 800, 675));
         primaryStage.show();
     }
 
