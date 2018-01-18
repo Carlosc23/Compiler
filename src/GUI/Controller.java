8ea65e0376642a8660ff3e51ab4374f45abd0fdb
Carlos <carlos.calderon097@gmail.com>
2018-01-18 00:10:06 -0600

    Added action to Compile.

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,4 +1,11 @@
 package GUI;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+    @FXML
+    private void compile(){
+        String filename = "C:\\Users\\Carlos Calderón\\IdeaProjects\\Compiler\\src\\Grammars\\Decaf.g4";
+        
+    }
 }
