f4ebaed16d4b7aad06fa16609f294411444e8efd
Carlos <carlos.calderon097@gmail.com>
2018-04-20 14:27:13 -0600

    feat: add static class Operator This class contain different number of operations.

---
+++ src/intermediate/Operator.java
@@ -0,0 +1,27 @@
+package intermediate;
+
+public class Operator {
+    public static final String LAB_FUNCSTART = "FUNC BEGIN";
+    public static final String LAB_FUNCEND = "FUNC END";
+    public static final String LAB_UMINUS = "UMINUS";
+    public static final String LAB_ASSIGN = "ASSIGN";
+    public static final String LAB_ADD = "ADD";
+    public static final String LAB_SUB = "SUB";
+    public static final String LAB_MULT = "MULT";
+    public static final String LAB_DIVIDE = "DIVIDE";
+    public static final String LAB_DIV = "DIV";
+    public static final String LAB_MOD = "MOD";
+    public static final String LAB_OR = "OR";
+    public static final String LAB_AND = "AND";
+    public static final String LAB_NOT = "NOT";
+    public static final String LAB_LESS = "LESS";
+    public static final String LAB_GREATER = "GREATER";
+    public static final String LAB_EQUALS = "EQUALS";
+    public static final String LAB_GOTO = "GOTO";
+    public static final String LAB_CALL = "CALL";
+    public static final String LAB_APARAM = "APARAM";
+    public static final String LAB_FPARAM = "FPARAM";
+    public static final String LAB_VAR = "VAR";
+    public static final String LAB_RETURN = "RETURN";
+    public  static final String LAB_IFZ = "IFZ";
+}
